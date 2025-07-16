39a5bce90d2b5444feea75203712f959b377f35c
Paul H <161055444+flopsllc@users.noreply.github.com>
2025-07-16 11:32:23 -0400

    added font compilation and cleaned up code style

---
+++ main.js
@@ -0,0 +1,106 @@
+#!/usr/bin/env node
+
+import path from 'path';
+import fs from 'fs';
+import readline from 'readline';
+
+import compile from './compile.js';
+import setupServer from './sync.js';
+import { parseSettings, bumpVersion } from './util.js';
+
+const runSettings = parseSettings( process.cwd() );
+
+const build = ( what ) => {
+
+	if ( what === 'css' ) {
+		return compile.css( runSettings.sourceIn, runSettings.destOut, runSettings.targetBrowsers ?? null );
+	} else if ( what === 'js' ) {
+		return compile.js( runSettings.sourceIn, runSettings.destOut, runSettings.targetBrowsers ?? null );
+	} else if ( what === 'fonts' ) {
+		return compile.js( runSettings.sourceIn, runSettings.destOut, runSettings.fontArgs ?? null );
+	}
+
+	return compile.all( runSettings.sourceIn, runSettings.destOut, runSettings );
+
+};
+
+const DONT_WATCH_LIST = [ 'version.php', 'package.json', 'package-lock.json' ];
+
+const watch = ( pathIn, callback ) => {
+
+	console.log( 'Watching for changes in ' + pathIn );
+
+	return fs.watch(
+		pathIn,
+		{
+			recursive: true,
+			persistent: true
+		},
+		callback
+	);
+
+};
+
+if ( runSettings.watch ) {
+	console.clear();
+	let server = null, app = null, page = null, io = null;
+	if ( runSettings.host ?? false ) {
+		setupServer( runSettings.host ).then( config => {
+			server = config.server;
+			app    = config.app;
+			page   = config.tab;
+			io     = config.io;
+		} ).catch( error => {
+			console.error( error );
+		} );
+	}
+	readline.emitKeypressEvents(process.stdin);
+	if ( process.stdin.isTTY ) {
+		process.stdin.setRawMode( true );
+	}
+
+	const watchDir = ( event, changedPath ) => {
+		if ( DONT_WATCH_LIST.includes( path.basename( changedPath ) ) ) {
+			return;
+		}
+		const extension = path.extname( changedPath );
+		const basename = path.basename( changedPath, extension );
+		let what = 'all';
+		if ( extension === '.map' ) {
+			return;
+		} else if ( basename.endsWith('.min') ) {
+			return;
+		} else if ( extension === '.css' ) {
+			what = 'css';
+		} else if ( extension === '.js' ) {
+			what = 'js';
+		}
+		build( what ).then( () => {
+			bumpVersion( runSettings.destOut );
+			console.log( `${changedPath} ${event} event, compilation of ${what} files complete` );
+			if ( io ) {
+				if ( what === 'js' ) {
+					io.emit( 'reload' );
+				}
+				if ( what === 'css' ) {
+					io.emit( 'refresh css', [ changedPath ] );
+				}
+			}
+		} ).catch( error => {
+			console.error( `${changedPath} ${event} event, error: ${error}` );
+		} );
+	};
+
+	watch( runSettings.sourceIn, watchDir );
+	process.stdin.on( 'keypress', ( str, key ) => {
+		if ( key.name === 'q' ) {
+			if ( server ) {
+				server.close();
+			}
+			process.exit();
+		}
+	} );
+	console.log( 'Press "q" to exit' );
+} else {
+	build( runSettings.build );
+}
---
+++ util.js
@@ -0,0 +1,150 @@
+import path from 'path';
+import fs from 'fs';
+import { parseArgs } from 'node:util';
+
+export const globalSettings = {
+	build:     'all',
+	watch:     false,
+	run:       false,
+	sourceIn:  path.join( process.cwd(), 'source' ),
+	destOut:   path.join( process.cwd(), 'dist' ),
+	replace:   false,
+	rootUrl:   '',
+	filenames: false,
+	locale:    'en-US'
+};
+
+export function updateGlobalSetting( key, value ) {
+	globalSettings[key] = value;
+}
+
+export function parseSettings( cwd ) {
+
+	const { values } = parseArgs( {
+		options: {
+			config: {
+				type:  'string',
+				short: 'c'
+			},
+			build: {
+				type:  'string',
+				short: 'b'
+			},
+			watch: {
+				type:  'boolean',
+			},
+			run: {
+				type:  'boolean',
+			},
+			in: {
+				type:  'string',
+				short: 'i'
+			},
+			out: {
+				type:  'string',
+				short: 'o'
+			},
+			replace: {
+				type:  'boolean',
+				short: 'r'
+			},
+			url: {
+				type:  'string',
+				short: 'u'
+			},
+			host: {
+				type:  'string',
+				short: 'h'
+			},
+			socket: {
+				type:  'string',
+				short: 's'
+			},
+			port: {
+				type:  'string',
+				short: 'p'
+			},
+			targetBrowsers: {
+				type:  'string'
+			}
+		},
+		tokens: true
+	} );
+
+	// check for config file and use as fallback defaults
+
+	for( let key in values ) {
+		if ( typeof values[key] === 'string' && values[key].startsWith( '=' ) ) {
+			values[key] = values[key].slice( 1 );
+		}
+	}
+
+	const packageFile = fs.readFileSync( path.join( process.cwd(), 'package.json' ) );
+	if ( packageFile ) {
+		const packageSettings = JSON.parse( packageFile );
+		if ( packageSettings && ( packageSettings.config?.bodykit ?? false ) ) {
+			for ( let key in packageSettings.config.bodykit ) {
+				values[key] = packageSettings.config.bodykit[ key ];
+			}
+		}
+	}
+
+	const newSettings = {
+		build:          values.build || null,
+		watch:          values.run || values.watch || null,
+		run:            values.run || null,
+		sourceIn:       path.join( cwd, values.in || 'source' ),
+		destOut:        path.join( cwd, values.out || values.in || 'dist' ),
+		replace:        values.replace || null,
+		rootUrl:        values.url || null,
+		filenames:      values.filenames || null,
+		host:           values.host || null,
+		port:           values.port || 3000,
+		socket:         values.port || 3001,
+		targetBrowsers: values.targetBrowsers || null,
+	};
+
+	for ( const key in newSettings ) {
+		if ( newSettings[key] !== null ) {
+			updateGlobalSetting( key, newSettings[key] );
+		}
+	}
+
+	return globalSettings;
+
+}
+
+/**
+ * Creates a PHP file returning the current timestamp as a version number.
+ */
+export function bumpVersion( destOut ) {
+
+	if ( ! destOut ) {
+		destOut = globalSettings.destOut;
+	}
+
+	const versionFile = path.join( destOut, 'version.php' );
+	const versionCode = `<?php\nreturn '1.0.${Date.now()}';\n`;
+
+	if ( fs.existsSync( path.join( destOut, 'package.json' ) ) ) {
+		const packageJson = JSON.parse( fs.readFileSync( path.join( destOut, 'package.json' ) ) );
+		if ( packageJson.version ) {
+			const version = packageJson.version.split( '.' );
+			if ( version.length > 2 ) {
+				version[ version.length - 1 ] = Date.now();
+			} else {
+				version.push( Date.now() );
+			}
+			packageJson.version = version.join( '.' );
+		}
+		fs.writeFileSync( path.join( destOut, 'package.json' ), JSON.stringify( packageJson, null, 2 ) );
+	}
+
+	fs.writeFileSync( versionFile, versionCode );
+
+}
+
+export default {
+	bumpVersion,
+	parseSettings
+}
